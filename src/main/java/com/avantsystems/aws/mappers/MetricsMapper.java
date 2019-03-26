ea16d1e45992675eab7cb9be66930c0d9d916319
Josh Uzo <nwankwo.joshua@gmail.com>
2019-03-26 15:52:01 +1100

    Added documentation

--- src/main/java/com/avantsystems/aws/mappers/MetricsMapper.java
+++ src/main/java/com/avantsystems/aws/mappers/MetricsMapper.java
@@ -9,10 +9,21 @@ import java.util.List;
 
 public class MetricsMapper {
 
+    /**
+     * Parses the timestamp section of the log lines into valid JDBC {@link Timestamp}
+     * @param columns
+     * @return
+     */
     private static String mapTimestamp(String[] columns){
         return columns[0] +" "+columns[1].split("\\,")[0] +"."+columns[1].split("\\,")[1];
     }
 
+    /**
+     * Transforms log lines into tokens.
+     *
+     * @param columns
+     * @return {@link QueryMetrics}
+     */
     public QueryMetrics buildMetrics(String[] columns){
         return new MetricsMapperBuilder()
                 .dateTime(Timestamp.valueOf(mapTimestamp(columns)))
@@ -23,7 +34,12 @@ public class MetricsMapper {
                 .create();
     }
 
-
+    /**
+     * Collates a list of already transformed {@link QueryMetrics}
+     *
+     * @param list log lines
+     * @return a {@link List} of {@link QueryMetrics}
+     */
     public List<QueryMetrics> map (List<String> list){
         List<QueryMetrics> qm = new ArrayList<>();
         for (String s: list) {
